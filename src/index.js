030f539080706539eafa494e74dc2973e6c33329
Luis Geniole <luis.geniole@gmail.com>
2021-03-13 22:52:24 -0300

    Add user creation.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header;
+
+  const userExists = users.find((user) => user.username === username);
+  if (!userExists)
+    return response.status(404).json({ error: 'Username taken.' });
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const usernameIsTaken = users.find((user) => user.username === username);
+  if (usernameIsTaken)
+    return response.status(400).json({ error: 'Username taken.' });
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
